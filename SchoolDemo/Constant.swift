4e3297b8611382efdfc19e18c24c10eb7ba89f97
Amel <amelsb45@gmail.com>
2023-03-15 17:36:11 -0400

    Initial Commit

---
+++ SchoolDemo/Constant.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  Created by Amel on 15/03/23.
+import Foundation
+
+ 
+var appName : String {
+    return Bundle.main.infoDictionary!["CFBundleName"] as! String
+}
+let AppDelegateObj : AppDelegate = AppDelegate.shared
+
+
+let BASE_URL = "https://data.cityofnewyork.us/resource/s3k6-pzi2.json"
+let BASE2_URL = "https://data.cityofnewyork.us/resource/f9bf-2cp4.json"
+
+class Constant:NSObject{
+ 
+    static let ImageBasePath = "https://image.tmdb.org/t/p/w500"
+    
+}
